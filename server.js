1614ac2471b77cf1ce999dff4bf12134a8b5b799
Erica M <emanoppo@users.noreply.github.com>
2016-08-23 21:06:46 -0400

    Add server.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-require('express');
+const port = '8080';
+const host = '0.0.0.0';
+const express = require('express');
+const app = express();
+
+app.listen(port, host);
+
+console.log('Server running on %s:%d...', host, port);
