ee08b192c3f57d048b365ce145c75945e6c990d1
Erica M <emanoppo@users.noreply.github.com>
2016-08-23 21:41:52 -0400

    Add index.js, connect to server.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const host = '0.0.0.0';
 const express = require('express');
 const app = express();
 
+const rules = require('./app');
+app.use(rules);
+
 app.listen(port, host);
 
 console.log('Server running on %s:%d...', host, port);
