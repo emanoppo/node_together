7ff82a68a890c3dcb152316f55b08959b9a5ca78
Erica M <emanoppo@users.noreply.github.com>
2016-08-24 08:44:22 -0400

    Add express-handlebars dependency, layouts

--- app/index.js
+++ app/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
+
+app.use(express.static('./app/public'));
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: './app/views/layouts'
+}));
+
+app.set('views', './app/views');
+app.set('view engine', 'handlebars');
 
 const motivations = require('motivations');
 const pickOne = require('pick-one');
 
 app.get('/', function(request, response) {
     random_motivation = pickOne(motivations);
-    response.send(random_motivation);
+    response.render('motivation', { motivation: random_motivation })
 });
 
 module.exports = app;
